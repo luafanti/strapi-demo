58099ce728b6842e5479c1f92e2a839ade81f459
Artur Bartosik <luafanti@gmail.com>
2022-12-22 09:55:27 +0100

    Add setup and config for PostgreSQL

--- config/database.js
+++ config/database.js
@@ -2,10 +2,16 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: env('DATABASE_CLIENT', 'postgres'),
+
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', true)
     },
-    useNullAsDefault: true,
+    debug: false
   },
 });
